39d92e7b229862b87c40b263b1571012dfaa4021
ilija <komperdel@gmail.com>
2022-08-02 18:23:24 +0300

    first commit

---
+++ src/components/app-filter/app-filter.js
@@ -0,0 +1,42 @@
+import './app-filter.css';
+
+const AppFilter = (props) => {
+	const buttonsData = [
+		{ name: 'all', lable: 'Все сотрудники' },
+		{ name: 'rise', lable: 'На повышение' },
+		{ name: 'moreThen1000', lable: 'З/П больше 1000$' },
+	];
+
+	const buttons = buttonsData.map(({ name, lable }) => {
+		const active = props.filter === name;
+		const clazz = active ? 'btn-light' : 'btn-outline-light';
+
+		return (
+			<button
+				type="button"
+				className={`btn ${clazz}`}
+				key={name}
+				onClick={() => props.onFilterSelect(name)}
+			>
+				{lable}
+			</button>
+		);
+	});
+
+	return (
+		<div className="btn-group">
+			{buttons}
+			{/* <button type="button" className="btn btn-light">
+				Все сотрудники
+			</button>
+			<button type="button" className="btn btn-outline-light">
+				На повышение
+			</button>
+			<button type="button" className="btn btn-outline-light">
+				З/П больше 1000$
+			</button> */}
+		</div>
+	);
+};
+
+export default AppFilter;
---
+++ src/components/employees-add-form/employees-add-form.js
@@ -0,0 +1,75 @@
+import { Component } from 'react';
+import './employees-add-form.css';
+
+class EmployeesAddForm extends Component {
+	state = {
+		name: '',
+		salary: '',
+	};
+
+	onValueChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	onSubmit = (e) => {
+		e.preventDefault();
+		if (this.state.name.length < 3 || !this.state.salary) return;
+		this.props.onAdd(this.state.name, this.state.salary);
+		this.setState({
+			name: '',
+			salary: '',
+		});
+		console.log(this.state);
+	};
+
+
+
+	// onNameValueChange = (e) => {
+	// 	this.setState({
+	// 		name: e.target.value,
+	// 	});
+	// };
+	// onSalaryValueChange = (e) => {
+	// 	this.setState({
+	// 		salary: e.target.value,
+	// 	});
+	// };
+
+	render() {
+		const { name, salary } = this.state;
+
+		return (
+			<div className="app-add-form">
+				<h3>Добавьте нового сотрудника</h3>
+				<form className="add-form d-flex" onSubmit={this.onSubmit}>
+					<input
+						type="text"
+						className="form-control new-post-label"
+						placeholder="Как его зовут?"
+						onChange={this.onValueChange}
+						name="name"
+						value={name}
+					/>
+					<input
+						type="number"
+						className="form-control new-post-label"
+						placeholder="З/П в $?"
+						onChange={this.onValueChange}
+						name="salary"
+						value={salary}
+					/>
+
+					<button type="submit" className="btn btn-outline-light">
+						Добавить
+					</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+
+
+export default EmployeesAddForm;
